696f86bab7858bed0c86e94730a80ac37ac44e5b
Malcolmston <mstone@crefeld.org>
2026-01-22 14:43:33 -0500

    feat: extend Class component to override state and support protected member references

--- public/components/class/Class.tsx
+++ public/components/class/Class.tsx
@@ -1,4 +1,19 @@
 import ObjectNode from "@/public/components/object/ObjectNode";
+import {ClassState} from "@/public/components/class/properties";
+import {MethodProps} from "@/public/components/Method";
+import Types from "@/public/components/objects";
+import React from "react";
 
 export default class Class extends ObjectNode {
+    private generatedGettersSetters: MethodProps[] = [];
+
+    // Override state type to include gettersSettersCollapsed
+    public state: ClassState;
+
+    // Access parent's protected members (they need to be protected in ObjectNode)
+    protected parmRefs: React.RefObject<SVGTextElement>[] = [];
+    // protected constantRefs: React.RefObject<SVGTextElement>[] = [];
+    protected conRefs: React.RefObject<SVGTextElement>[] = [];
+    protected methodRefs: React.RefObject<SVGTextElement>[] = [];
+    protected classType: Types = Types.CLASS;
 }
