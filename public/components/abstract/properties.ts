f8ec2916e966708dc44dbc07e8300770c05bfb6b
Malcolmston <mstone@crefeld.org>
2026-01-22 16:40:50 -0500

    refactor: improve Props type imports for abstract and class components

--- public/components/abstract/properties.ts
+++ public/components/abstract/properties.ts
@@ -1,3 +1,7 @@
+import {ParmProps} from "@/public/components/Parameter";
+import {ConstructorProps} from "@/public/components/Constructor";
+import {MethodProps} from "@/public/components/Method";
+
 type Props = {
     name: string;
     x: number;
