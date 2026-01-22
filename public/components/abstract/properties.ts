84fcfd7f07160131a771415ef9aa34c0dcdf66cd
Malcolmston <mstone@crefeld.org>
2026-01-22 16:39:14 -0500

    feat: add Props type definition to abstract components

---
+++ public/components/abstract/properties.ts
@@ -0,0 +1,11 @@
+type Props = {
+    name: string;
+    x: number;
+    y: number;
+
+    params?: ParmProps[];
+    constructors?: ConstructorProps[];
+    methods?: MethodProps[];
+};
+
+export default Props;
