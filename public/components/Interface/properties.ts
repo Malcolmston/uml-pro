8afda7403bce2665fe3487c586c2986c039bd92c
Malcolmston <mstone@crefeld.org>
2026-01-22 17:22:15 -0500

    feat: add Props type definition for Interface components

---
+++ public/components/Interface/properties.ts
@@ -0,0 +1,13 @@
+import {ParmProps} from "@/public/components/Parameter";
+import {ConstructorProps} from "@/public/components/Constructor";
+import {MethodProps} from "@/public/components/Method";
+
+type Props = {
+    name: string;
+    x: number;
+    y: number;
+
+    params?: ParmProps[];
+    constructors?: ConstructorProps[];
+    methods?: MethodProps[];
+};
