be84bf149abc67689ea389b42bce4136e3ffbea5
Malcolmston <mstone@crefeld.org>
2026-01-22 12:51:17 -0500

    feat: add Types enum for UML object definitions

---
+++ public/components/objects.ts
@@ -0,0 +1,10 @@
+enum Types {
+    CLASS = '',
+    ABSTRACT = "abstract",
+    INTERFACE = "interface",
+    ENUM = "enumeration",
+    ANNOTATION = "annotation",
+    RECORD = "record"
+}
+
+export default Types;
