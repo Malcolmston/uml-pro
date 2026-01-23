e398190083f33eaaabca2e2b711831771377e73e
Malcolmston <mstone@crefeld.org>
2026-01-22 20:52:32 -0500

    refactor: add `CreateAbstractProps` and `CreateAbstractState` types to support abstract window functionality

---
+++ public/components/window/abstract/properties.ts
@@ -0,0 +1,32 @@
+import ParamInput from "@/public/components/window/param_input";
+import ConstructorInput from "@/public/components/window/constructor_input";
+import MethodInput from "@/public/components/window/method_input";
+
+export type AbstractMethod = {
+    isAbstract?: boolean;
+} & MethodInput;
+
+export interface CreateAbstractProps {
+    onAdd: (node: React.JSX.Element) => void;
+    onClose?: () => void;
+    initialData?: {
+        className?: string;
+        params?: ParamInput[];
+        methods?: AbstractMethod[];
+        constructors?: ConstructorInput[];
+        autoGettersSetters?: boolean;
+    };
+}
+
+export interface CreateAbstractState {
+    className: string;
+    params: ParamInput[];
+    constructors: ConstructorInput[];
+    methods: AbstractMethod[];
+    errors: Record<string, string>;
+    editingParam: string | null;
+    editingMethod: string | null;
+    paramDraft: Omit<ParamInput, 'id'>;
+    methodDraft: Omit<AbstractMethod, 'id'>;
+    autoGettersSetters: boolean;
+}
