b994f3dc5107c97a2ecbf27fbcd18aff72f1d842
Malcolmston <mstone@crefeld.org>
2026-01-23 10:49:52 -0500

    feat: add `Team` entity to database schema
    - Introduced `Team` entity with properties: `id`, `role`, `name`, `createdAt`, and `updatedAt`.
    - Utilized TypeORM decorators for entity definition and timestamps.

---
+++ app/db/entities/Team.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, BeforeInsert, BeforeUpdate } from "typeorm"
+
+@Entity()
+export class Team {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({
+        type: "enum",
+        enum: TeamRole,
+        default: TeamRole.MEMBER
+    })
+    role: TeamRole;
+
+    @Column({nullable: false})
+    name: string
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
