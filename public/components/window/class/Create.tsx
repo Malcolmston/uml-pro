15f50ae0e8c390922ffe55d05825c6a28a654da0
Malcolmston <mstone@crefeld.org>
2026-01-22 19:41:42 -0500

    feat: initialize `idRef` and update `CreateClass` state with default values

--- public/components/window/class/Create.tsx
+++ public/components/window/class/Create.tsx
@@ -1,5 +1,36 @@
 import React from "react";
 import {CreateClassProps, CreateClassState} from "@/public/components/window/class/properties";
+import Visibility from "@/public/components/visibility";
 
 export default class CreateClass extends React.Component<CreateClassProps, CreateClassState> {
+    idRef: { current: number };
+
+
+    constructor(props: CreateClassProps) {
+        super(props);
+        this.idRef = { current: 0 };
+        this.state = {
+            className: "",
+            params: [],
+            constructors: [],
+            methods: [],
+            errors: {},
+            editingParam: null,
+            editingMethod: null,
+            paramDraft: {
+                name: "",
+                type: "",
+                visibility: Visibility.PRIVATE,
+                isStatic: false,
+                isFinal: false
+            },
+            methodDraft: {
+                name: "",
+                returnType: "",
+                visibility: Visibility.PUBLIC,
+                isStatic: false
+            },
+            autoGettersSetters: false
+        };
+    }
 }
