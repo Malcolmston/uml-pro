83275c16ce21c1312f12a8f1e4512d61d38cc037
Malcolmston <mstone@crefeld.org>
2026-01-23 16:07:40 -0500

    refactor: update `createBucket` and `bucketExists` to use async/await with improved return types

--- app/utils/s3.ts
+++ app/utils/s3.ts
@@ -13,29 +13,32 @@ const supabase = createClient(
  * Creates a new storage bucket with the specified name.
  *
  * @param {string} name - The name of the bucket to be created. It must be a non-empty string.
- * @return {boolean} Returns true if the bucket is successfully created, or false if a bucket with the specified name already exists.
+ * @return {Promise<{data: {name: string}, error: null} | {data: null, error: Error}>} Returns the created bucket or error.
  * @throws {Error} Throws an error if the bucket name is not provided.
  */
-function createBucket(name: string): boolean {
+async function createBucket(name: string) {
     if (!name) {
         throw new Error('Bucket name is required')
     }
 
-    if (bucketExsists(name)) {
-        return false
+    const { data, error } = await supabase.storage.createBucket(name)
+
+    if (error) {
+        return { data: null, error }
     }
 
-    return supabase.storage.createBucket(name)
+    return { data, error: null }
 }
 
 /**
  * Checks if a bucket with the specified name exists.
  *
  * @param {string} name - The name of the bucket to check.
- * @return {boolean} Returns true if the bucket exists, false otherwise.
+ * @return {Promise<boolean>} Returns true if the bucket exists, false otherwise.
  */
-function bucketExsists(name: string): boolean {
-    return supabase.storage.getBucket(name) !== null
+async function bucketExsists(name: string): Promise<boolean> {
+    const { data } = await supabase.storage.getBucket(name)
+    return data !== null
 }
 
 /**
