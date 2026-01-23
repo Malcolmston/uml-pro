cad74d6f986d098a18b5e3b8cc6306af15385aba
Malcolmston <mstone@crefeld.org>
2026-01-22 19:42:34 -0500

    feat: add `componentDidUpdate` to handle `initialData` prop updates and update state accordingly

--- public/components/window/class/Create.tsx
+++ public/components/window/class/Create.tsx
@@ -33,4 +33,27 @@ export default class CreateClass extends React.Component<CreateClassProps, Creat
             autoGettersSetters: false
         };
     }
+
+    /**
+     * Lifecycle method invoked immediately after updating occurs.
+     * It checks if the `initialData` prop has changed after the component has mounted,
+     * and updates the state accordingly to populate relevant data fields.
+     *
+     * @param {CreateClassProps} prevProps The previous properties received by the component before the update.
+     * @return {void} Does not return a value.
+     */
+    componentDidUpdate(prevProps: CreateClassProps) {
+        // Autopopulate if initialData changes after mount
+        if (
+            this.props.initialData && this.props.initialData !== prevProps.initialData
+        ) {
+            const { initialData } = this.props;
+            if (initialData.className) this.setState({ className: initialData.className });
+            if (initialData.params) this.setState({ params: initialData.params });
+            if (initialData.methods) this.setState({ methods: initialData.methods });
+            if (initialData.constructors) this.setState({ constructors: initialData.constructors });
+            if (typeof initialData.autoGettersSetters === 'boolean') this.setState({ autoGettersSetters: initialData.autoGettersSetters });
+        }
+    }
+
 }
