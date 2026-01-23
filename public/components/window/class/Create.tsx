f50325212edb0ef0688c6511545a61f7a3c8bb2f
Malcolmston <mstone@crefeld.org>
2026-01-22 19:37:04 -0500

    feat: add `CreateClass` component to manage class creation structure

---
+++ public/components/window/class/Create.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import {CreateClassProps, CreateClassState} from "@/public/components/window/class/properties";
+
+export default class CreateClass extends React.Component<CreateClassProps, CreateClassState> {
+}
