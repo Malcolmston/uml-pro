cb813d9a5de3230fc03c204469d796c05106c994
Malcolmston <mstone@crefeld.org>
2026-01-23 10:30:25 -0500

    feat: add password hashing and age validation to `User` entity
    - Implemented `@BeforeInsert` hook to enforce a minimum age of 12 for registration.
    - Added password hashing logic using `bcrypt` for enhanced security.
    - Introduced static methods `hashPassword` and `comparePassword` for password-related operations.

--- app/db/entities/User.ts
+++ app/db/entities/User.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
@@ -22,4 +23,37 @@ export class User {
 
   @Column({nullable: false})
   password: string
+
+  @BeforeInsert()
+  async beforeInsert() {
+    if (this.age < 12) {
+      throw new Error('User must be at least 12 years old to register');
+    }
+
+    this.password = await this.hashPassword();
+  }
+
+
+
+  /**
+   * Generates a hashed version of the given plain-text password.
+   *
+   * @param password The plain-text password to be hashed.
+   * @return A promise that resolves to the hashed password.
+   */
+  private static async hashPassword(password: string): Promise<string> {
+      return await bcrypt.hash(password, 10);
+  }
+
+
+  /**
+   * Compares a plaintext password attempt with a hashed password to determine if they match.
+   *
+   * @param {string} attempt - The plaintext password input to compare.
+   * @param {string} hash - The hashed password to compare against.
+   * @return {Promise<boolean>} A promise that resolves to `true` if the passwords match, otherwise `false`.
+   */
+  private static async comparePassword(attempt: string, hash: string): Promise<boolean> {
+      return await bcrypt.compare(attempt, hash);
+  }
 }
